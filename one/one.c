12fd17fb3b2bed44c111a3ab165ae69871e0213a
lycodestore <353761948@qq.com>
2021-04-24 18:33:44 +0800

    在CLion中使用ffmpeg

---
+++ one/one.c
@@ -0,0 +1,13 @@
+//
+// Created by ly on 2021/4/24.
+//
+
+#include "one.h"
+
+int printHello() {
+    printf("hello world");
+}
+
+int printHello2() {
+    printf("hello world2");
+}
---
+++ one/one.h
@@ -0,0 +1,13 @@
+//
+// Created by ly on 2021/4/24.
+//
+
+#ifndef FFSTUDIO_ONE_H
+#define FFSTUDIO_ONE_H
+#include <stdio.h>
+
+int printHello();
+
+int printHello2();
+
+#endif //FFSTUDIO_ONE_H
